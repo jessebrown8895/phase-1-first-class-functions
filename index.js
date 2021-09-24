8c59aeced57d16b83e96cb302ed173945f739502
Jesse Brown <Brownjesse8895@yahoo.com>
2021-09-24 12:23:30 -0500

    commit tree 9f0926100446ecea601ae190a1296783f12fffd7

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+
+function receivesAFunction(callback){
+    return callback()
+}
+
+receivesAFunction(function() {"spy"})
+
+function returnsANamedFunction(){
+        return function namedFunction(){
+            "returning something"
+        }
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+        "I am an anonymous function"
+    }
+}
